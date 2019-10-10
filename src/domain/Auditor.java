2e3b7b0bea44edea12113d535354f78b97b7d9f7
Tomas <tomadomavicius@gmail.com>
2019-10-10 14:07:25 +0200

    auditor class

--- src/domain/Auditor.java
+++ src/domain/Auditor.java
@@ -2,10 +2,17 @@ package domain;
 
 public class Auditor implements Observer,Console {
     private Account a;
+    private Bank bank;
+
+    public Auditor(Bank bank) {
+        this.bank = bank;
+        bank.registerObserver(this);
+    }
 
     @Override
     public void update(Account a) {
         this.a = a;
+        display();
     }
 
     @Override
