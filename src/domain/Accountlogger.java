0da4b358d074e8b5d07d4f98e9ad064da19a3005
Tomas <tomadomavicius@gmail.com>
2019-10-10 14:09:50 +0200

    account logger class

--- src/domain/Accountlogger.java
+++ src/domain/Accountlogger.java
@@ -1,4 +1,23 @@
 package domain;
 
-public class Accountlogger {
+public class Accountlogger implements Observer, Console {
+    private Bank bank;
+    private Account a;
+
+    public Accountlogger(Bank bank)
+    {
+        this.bank = bank;
+        bank.registerObserver(this);
+    }
+
+    @Override
+    public void display() {
+        System.out.println(a.toString());
+    }
+
+    @Override
+    public void update(Account a) {
+        this.a = a;
+        display();
+    }
 }
