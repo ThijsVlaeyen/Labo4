ebaca70d86a0590318ccde2707bb3f362067218a
Tomas <tomadomavicius@gmail.com>
2019-10-10 13:51:15 +0200

    observer and subject interfaces

--- src/domain/Observer.java
+++ src/domain/Observer.java
@@ -1,4 +1,5 @@
 package domain;
 
 public interface Observer {
+    public void update(Account a);
 }
--- src/domain/Subject.java
+++ src/domain/Subject.java
@@ -1,4 +1,7 @@
 package domain;
 
 public interface Subject {
+    public void registerObserver(Observer o);
+    public void removeObserver(Observer o);
+    public void updateObsrvers();
 }
